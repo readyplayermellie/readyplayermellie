4ed7abdea0a5344abd2c265c0b94c8b3e2cc0f99
Gast <gast@MacBook-Pro-van-Lars.local>
2024-02-28 11:24:05 +0100

    Moved icons to assets folder

--- components/Sticker.vue
+++ components/Sticker.vue
@@ -1,6 +1,6 @@
 <template>
     <NuxtLink class="group sticker w-96 flex" :to="content.link" :target="content.external ? '_blank' : '_self'">
-        <img class="mr-4 group-hover:ml-2" src="/icons/alien.png">
+        <img class="mr-4 group-hover:ml-2" src="~/assets/icons/alien.png">
         <p class="title">{{ content.title }}</p>
     </NuxtLink>
 </template>
